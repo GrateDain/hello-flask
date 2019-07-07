a6ca9e6fe3cfe5019a7b2518da612e7791bdad6c
GrateDane <samjdainard@gmail.com>
2019-07-07 18:47:23 -0500

    End of class 6, templates and linking index page added

--- main.py
+++ main.py
@@ -1,29 +1,89 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
 
 app = Flask (__name__)
 app.config['DEBUG'] = True
 
-
-form = """
-<!doctype html>
-<html>
-  <body>
-    <form action="/hello" method="POST">
-      <label for="first_name">First name:</label> 
-      <input type="text" name="first_name" id="first_name"/>
-      <input type="submit" />
-    </form>
-  </body>
-</html>
-"""
-
-@app.route("/")
+@app.route('/')
 def index():
-    return form
+    return render_template('base.html', title='Index Page')
+
+@app.route("/hello")
+def hello():
+    return render_template('hello_form.html', title='Hello Form')
 
 @app.route("/hello", methods=["POST"])
-def hello():
+def hello_reply():
     first_name = request.form['first_name']
-    return '<h1>Hello, ' + first_name + '</h1>'
+    return render_template('hello_reply.html', title='Hello Reply', name=first_name)
+
+@app.route('/validate-time')
+def display_time_form():
+    return render_template('time_form.html', title='Time Validation')
+
+def is_int(num):
+  try:
+    int(num)
+    return True
+  except ValueError:
+    return False
+
+
+@app.route('/validate-time', methods=['POST'])
+def validate_time():
+    hours = request.form['hours']
+    minutes = request.form['minutes']
+
+    hours_error = ''
+    minutes_error = ''
+
+    if not is_int(hours):
+      hours_error = "Not a valid integer"
+      hours=''
+    else:
+      hours = int(hours)
+      if hours > 23 or hours < 0:
+        hours_error = 'Hour value out of range (0-23)'
+        hours=''
+
+    if not is_int(minutes):
+      hours_error = "Not a valid integer"
+      minutes_error=''
+    else:
+      minutes = int(minutes)
+      if minutes > 59 or minutes < 0:
+        minutes_error = 'Minutes value out of range (0-59)'
+        minutes=''
+    
+    if not minutes_error and not hours_error:
+      if minutes < 10:
+        minutes = '0' + str(minutes)
+      time = str(hours) + ':' + str(minutes)
+      return redirect('/valid-time?time={0}'.format(time))
+    else:
+      return render_template('time_form.html', 
+      title='Time Validation',
+      style='.error { color: red; }',
+      hours_error=hours_error, 
+      minutes_error=minutes_error, 
+      hours=hours, 
+      minutes=minutes)
+
+@app.route('/valid-time')
+def valid_time():
+  time = request.args.get('time')
+  return '<h1>Thanks for submitting, {0} a valid time!</h1>'.format(time)
+
+tasks = []
+
+@app.route('/todos', methods=['POST', 'GET'])
+def todos():
+
+  if request.method == 'POST':
+      task = request.form['task']
+      tasks.append(task)
+
+  return render_template('todos.html', title='TODOs', tasks=tasks)
 
 app.run()
